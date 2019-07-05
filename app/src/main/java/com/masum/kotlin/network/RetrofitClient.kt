b3add339e6b75fd520f91c3f30df2504c39bf099
mdmasum.shuvo.cse@gmail.com <mdmasum.shuvo.cse@gmail.com>
2019-07-06 00:37:43 +0600

    c

--- app/src/main/java/com/masum/kotlin/network/RetrofitClient.kt
+++ app/src/main/java/com/masum/kotlin/network/RetrofitClient.kt
@@ -6,27 +6,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 //singleTone class for retrofit
 class RetrofitClient {
 
-   /* private lateinit var retrofitClient: Retrofit
-
-    var apiService: ApiService? = null
-
-    @Volatile
-    private var restclient: RetrofitClient? = null*/
-
     companion object {
         private const val BASE_URL = "http://maxproit.solutions/datasync/api/"
     }
-
-/*    init {
-        if (!::retrofitClient.isInitialized) {
-            retrofitClient = Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-        }
-
-        apiService = retrofitClient.create(ApiService::class.java)
-    }*/
 
 
     val instance: ApiService by lazy {
@@ -34,8 +16,6 @@ class RetrofitClient {
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-
-
         retrofit.create(ApiService::class.java)
     }
 
