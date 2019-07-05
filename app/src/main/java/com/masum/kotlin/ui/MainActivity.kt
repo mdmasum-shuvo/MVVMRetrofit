42528bd95de255cba05fb3c4866c379faf670e98
mdmasum.shuvo.cse@gmail.com <mdmasum.shuvo.cse@gmail.com>
2019-07-05 23:28:51 +0600

    c

--- app/src/main/java/com/masum/kotlin/ui/MainActivity.kt
+++ app/src/main/java/com/masum/kotlin/ui/MainActivity.kt
@@ -1,33 +1,28 @@
 package com.masum.kotlin.ui
 
 import android.arch.lifecycle.Observer
-import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.Toast
 import com.masum.kotlin.R
 import com.masum.kotlin.common.BaseActivity
-import com.masum.kotlin.datamodel.ResponseModel
+import com.masum.kotlin.databinding.ActivityMainBinding
 import com.masum.kotlin.listener.ServerRequestFailedListener
-import com.masum.kotlin.network.RetrofitClient
 import com.masum.kotlin.viewmodel.ViewModel
 import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import java.util.*
 
 
-class MainActivity : BaseActivity(), ServerRequestFailedListener {
-    override fun getLaoyutId(): Int {
-       return R.layout.activity_main
+open class MainActivity : BaseActivity(), ServerRequestFailedListener {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun getLayoutResource(): Int {
+        return R.layout.activity_main
+    }
+
+    override fun initComponent() {
+        binding = getBinding as ActivityMainBinding
     }
 
     override fun functionality() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun listener() {
@@ -38,37 +33,13 @@ class MainActivity : BaseActivity(), ServerRequestFailedListener {
             })
 
             viewModel.repository.setListener(this)
-        }    }
-
+        }
+    }
 
 
     override fun onFailed(massage: String) {
-      toasmsg(massage)
+        showAllertDialuge(massage)
     }
-
-    /*
-        fun btnView(view: View) {
-
-
-            //val apiService = retrofit.apiService
-
-            RetrofitClient().instance.getResponse().enqueue(object : Callback<List<ResponseModel>> {
-                override fun onFailure(call: Call<List<ResponseModel>>, t: Throwable) {
-                    Log.e("", "")
-                }
-
-                override fun onResponse(call: Call<List<ResponseModel>>, response: Response<List<ResponseModel>>) {
-                    Log.e("", "")
-                }
-
-            })
-
-
-        }*/
-    fun toasmsg(msg: String) {
-        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
-    }
-
 
 }
 
